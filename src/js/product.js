58ccacc3d95da6dec85a23a3c7109f9d67fd3dbe
TXzzera <brunoteixeira.10@hotmail.com>
2025-07-20 19:13:27 -0300

    att

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,5 @@
 // product.js
-import {getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
