43c59d2bf32de57e2b6154581637405bdc01ced2
TXzzera <brunoteixeira.10@hotmail.com>
2025-07-21 21:37:20 -0300

    att

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,21 @@
-// product.js
-import { getParam } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
 
-const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
-const productDetails = new ProductDetails(productId, dataSource);
-productDetails.init();
+function addProductToCart(product) {
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
+}
+
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
